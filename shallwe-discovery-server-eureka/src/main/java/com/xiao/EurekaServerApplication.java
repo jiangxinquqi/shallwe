6a8f5eaea9eeadbd316d4e6fa0454f3e6e474cb2
xiaojianjun <xiaoxiao65535@163.com>
2021-08-04 02:06:57 +0800

    配置github-webhook

--- shallwe-discovery-server-eureka/src/main/java/com/xiao/EurekaServerApplication.java
+++ shallwe-discovery-server-eureka/src/main/java/com/xiao/EurekaServerApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
